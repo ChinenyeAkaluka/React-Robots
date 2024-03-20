39a4882d265bfc31aaaae99421900d4c8299dffe
ChinenyeAkaluka <chico_aka@yahoo.com>
2024-03-19 23:01:39 -0500

    Added all files

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import './App.css';
+import CardList from './components/card-list/card-list.conponent';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      robots:[],
+      searchField: ''
+    };
+  }
+
+   componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( (response) => response.json() )
+    .then( (users) => 
+      this.setState(
+        () => {
+          return { robots: users};
+        }
+      ));
+   }
+
+   onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState( () => {
+      return {searchField};
+    });
+   }
+
+  render(){
+    const { robots, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredRobots = robots.filter( (robot) => {
+      return robot.name.toLocaleLowerCase().includes(searchField);
+    });
+
+
+    return (
+      <div className="App">
+        <h1 className='app-title'> Robots Rolodex </h1>
+        <SearchBox onChangeHandler= {onSearchChange}
+                   placeholder= 'search robots' 
+                   className= 'search-box' />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
